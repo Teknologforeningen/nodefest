9b71cb0eaceddfd3316bc60ad9823314424cd5d1
Oleg Stikhin <stikhio1@niksula.hut.fi>
2016-02-15 02:18:31 +0200

    submit view

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
 var router = express.Router();
 
 /* GET home page. */
@@ -12,6 +13,23 @@ router.get('/register/', function(req, res, next) {
 
 /* POST submit form */
 router.post('/submit/', function(req, res, next) {
+  console.log(req.body);
+  var transporter = nodemailer.createTransport('smtp://smtp.ayy.fi');
+  
+  var mailOptions = {
+    from: 'TF144 <arsfest@teknolog.fi>',
+    to: 'oleg.stikhin@aalto.fi',
+    subject: 'Bekräftelse',
+    text: 'Din anmälan har mottagits' + req.body.first_name,
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      res.send(error);
+    } else {
+      console.log('Message sent: ' + info.response);
+    }
+  });
   res.render('submit', { title: 'Bekräftelse' });
 });
 
